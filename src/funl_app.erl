53dea1a54a2c4b488eac8d85b632f0fc45a7bf02
Adi Ciausu <adi@192-168-0-106.rdsnet.ro>
2016-12-03 12:23:58 +0200

    route all requests to pending

--- src/funl_app.erl
+++ src/funl_app.erl
@@ -15,7 +15,7 @@ start(_Type, _Args) ->
 start_http_listener() ->
   Dispatch = cowboy_router:compile([
     {'_', [
-      {"/", funl_handler_pending, []}
+      {'_', funl_handler_pending, []}
     ]}
   ]),
   {ok, _} = cowboy:start_http(http, 100, [{port, 8080}], [
