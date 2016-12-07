5fd01313c9984f2b6784f306b9f944de6212f176
Adi Ciausu <adi@192-168-0-106.rdsnet.ro>
2016-12-07 23:13:07 +0200

    request ttl tagging

--- src/funl_http_listener.erl
+++ src/funl_http_listener.erl
@@ -3,20 +3,26 @@
 -export([init/2]).
 
 init(Req, Opts) ->
-    Request = #request{wrapped_request = Req, received_at = funl_uid:timestamp()},
-    Ttl = cowboy_req:header(<<"x-funl-ttl">>, Req),
-    case is_binary(Ttl) of
-        true -> funl_timed_queue:enq(Request#request{ttl = binary_to_integer(Ttl)}, funl_uid:timestamp());
-        _ -> funl_timed_queue:enq(Request, funl_uid:timestamp())
-    end,
-    
-    
-    
+    funl_timed_queue:enq(wrap_request(Req), funl_uid:timestamp()),
     Req2 = cowboy_req:reply(200, [
         {<<"content-type">>, <<"text/plain">>}
     ], <<"Request queued!">>, Req),
     {ok, Req2, Opts}.
 
+wrap_request(Req) ->
+    Request = #request{wrapped_request = Req, received_at = funl_uid:timestamp()},
+    Ttl = cowboy_req:header(<<"x-funl-ttl">>, Req),
+    case is_binary(Ttl) of
+        true ->
+            case re:run(Ttl, "^[0-9]*$") of
+                nomatch -> Request;
+                {match, _} -> Request#request{ttl = binary_to_integer(Ttl)}
+            end;
+        _ -> Request
+    end.
+
+    
+      
 
 
     
